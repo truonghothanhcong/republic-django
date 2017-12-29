4895ef02e5510902dea26924f648c0cbe7186895
cong <truonghothanhcong@gmail.com>
2017-12-29 11:23:50 +0700

    config dependency.sh + database

---
+++ republic/dependency.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install g++
+sudo apt-get install python-pip
+sudp apt-get install python-dev
+sudo apt-get install python-flup
+sudo apt-get install mysql-client-core-5.5
+sudo apt-get install libmysqlclient-dev
+
+sudo pip install Django==1.11
+sudo pip install mysql-python
+sudo pip install requests
+sudo pip install mysqlclient==1.3.12
